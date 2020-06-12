3b451e2024f6f3ee5566276311011bd32bfafb7c
akshay-varun <akshaygr4@gmail.com>
2020-06-12 13:17:07 +0530

    Done with Firebase.

---
+++ public/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDwE0CZQFxHsFkqZsMYLM6vZ97DDU0E4TI",
+    authDomain: "adwebsite-42840.firebaseapp.com",
+    databaseURL: "https://adwebsite-42840.firebaseio.com",
+    projectId: "adwebsite-42840",
+    storageBucket: "adwebsite-42840.appspot.com",
+    messagingSenderId: "490886765130",
+    appId: "1:490886765130:web:269b136933d169736ce4b3",
+    measurementId: "G-SSBG32R60X"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
