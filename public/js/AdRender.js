b68bac0997bc6e667566610e174faf0a2da588ce
akshay-varun <akshaygr4@gmail.com>
2020-06-19 11:33:03 +0530

    Done with aligning ads.

---
+++ public/js/AdRender.js
@@ -0,0 +1,11 @@
+function getRandomImage() {
+    var images = ["../images/ads/FRUITS.png", "../images/ads/SPORTS.png"];
+    var image = images[Math.floor(Math.random()*images.length)];
+    return image;
+}
+
+function displayRandomImage() {
+    var htmlImage = document.getElementById("banner1");
+    htmlImage.src = getRandomImage();
+    return
+}
